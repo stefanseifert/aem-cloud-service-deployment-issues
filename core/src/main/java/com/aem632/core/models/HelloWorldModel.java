e23313fa5fb4e2ad7f3ff10ef57880ccba729864
Stefan Seifert <stefanseifert@users.noreply.github.com>
2021-04-14 08:59:44 +0200

    ouptut maven/java version and system environment information on maven build output runtime system environment variables in helloworld model

--- core/src/main/java/com/aem632/core/models/HelloWorldModel.java
+++ core/src/main/java/com/aem632/core/models/HelloWorldModel.java
@@ -17,6 +17,8 @@ package com.aem632.core.models;
 
 import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
 
+import java.util.Optional;
+
 import javax.annotation.PostConstruct;
 
 import org.apache.sling.api.resource.Resource;
@@ -24,14 +26,11 @@ import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
-
-import java.util.Optional;
 
 @Model(adaptables = Resource.class)
 public class HelloWorldModel {
@@ -54,9 +53,19 @@ public class HelloWorldModel {
                 .map(pm -> pm.getContainingPage(currentResource))
                 .map(Page::getPath).orElse("");
 
-        message = "Hello World!\n"
-            + "Resource type is: " + resourceType + "\n"
-            + "Current page is:  " + currentPagePath + "\n";
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Hello World!\n")
+          .append("Resource type is: ").append(resourceType).append("\n")
+          .append("Current page is:  ").append(currentPagePath).append("\n")
+          .append("\n")
+          .append("System environment variables\n")
+          .append("----------------------------\n");
+        System.getProperties().entrySet().forEach(entry -> {
+          sb.append(entry.getKey()).append(" = ").append(entry.getValue()).append("\n");
+        });
+
+        this.message = sb.toString();
     }
 
     public String getMessage() {
