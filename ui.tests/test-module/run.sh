4e7a86a60e925358a0413c58ca0c006f59f9ecb9
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-11-16 13:36:46 +0100

    regenerate project with archetype 45

--- ui.tests/test-module/run.sh
+++ ui.tests/test-module/run.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Copyright 2023 Adobe Systems Incorporated
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,20 +14,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-FROM cypress/included:12.17.0
-
-ENV APP_PATH /usr/src/app
-RUN apt -qqy update \
-    # Generic dependencies
-    && apt -qqy --no-install-recommends install \
-    python \
-    build-essential
-# <<< End EAAS Convention \
-
-# Set Application Environment
-WORKDIR ${APP_PATH}
-COPY ./test-module ./
-RUN npm install
-# no need to wait for selenium
-ENTRYPOINT ["bash","run.sh"]
-
+# start a single X11 server and pass the server's address to each Cypress instance using DISPLAY variable
+pkill Xvfb
+echo 'start xvfb'
+Xvfb :99 -screen 0 1280x1024x24 -ac -nolisten tcp -nolisten unix &
+export DISPLAY=:99
+echo 'checking Xvfb'
+ps aux | grep Xvfb
+# disable color output when running Cypress
+export NO_COLOR=1
+# execute tests
+npm test
